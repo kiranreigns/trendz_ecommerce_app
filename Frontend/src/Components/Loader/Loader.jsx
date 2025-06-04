743dbfce34de5f834ad35cb295b07c5c5935527f
Kiran <kiranreigns@hotmail.com>
2025-06-04 17:26:18 +0530

    Create loading animation

---
+++ Frontend/src/Components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loading-container">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loader;
