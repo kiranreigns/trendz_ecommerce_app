561855940b1de0f4fd247c050e5835f03dbef22d
Kiran <kiranreigns@hotmail.com>
2025-06-08 22:41:57 +0530

    Fix file naming conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./Context/AuthContext";
-import ShopContextProvider from "./Context/ShopContextProvider";
+import { AuthProvider } from "./context/AuthContext";
+import ShopContextProvider from "./context/ShopContextProvider";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
